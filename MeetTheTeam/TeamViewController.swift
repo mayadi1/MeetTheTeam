01d173e080c7754ca77e60e3ac19c3a3c419197b
mayadi1 <admin@ayadios.com>
2016-12-21 02:46:38 -0800

    collection view added

--- MeetTheTeam/TeamViewController.swift
+++ MeetTheTeam/TeamViewController.swift
@@ -8,13 +8,39 @@
 
 import UIKit
 
-class TeamViewController: UIViewController {
+class TeamViewController: UIViewController, UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
     var passedPersons = [TeamPerson]()
+    var collectionView: UICollectionView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        //self.title = "Meet The Team"
+        initializeCollectionView()
 
-    
     }
-
+    
+    //Collection View initialization
+    func initializeCollectionView() {
+        let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: 20, left: 10, bottom: 10, right: 10)
+        layout.itemSize = CGSize(width: 70, height: 70)
+        
+        let collectionView:UICollectionView = UICollectionView(frame: self.view.frame, collectionViewLayout: layout)
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "Cell")
+        collectionView.backgroundColor = UIColor.white
+        self.view.addSubview(collectionView)
+    }
+    
+    //Collection View Setup Delegate and datasource
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return passedPersons.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath)
+        cell.backgroundColor = UIColor.lightGray
+        return cell
+    }
 }
