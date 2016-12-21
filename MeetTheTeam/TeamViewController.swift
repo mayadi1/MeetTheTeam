dd61b3546d26150731beb98a2ae01e082331a45e
mayadi1 <admin@ayadios.com>
2016-12-21 03:46:11 -0800

    tableView added

--- MeetTheTeam/TeamViewController.swift
+++ MeetTheTeam/TeamViewController.swift
@@ -8,39 +8,37 @@
 
 import UIKit
 
-class TeamViewController: UIViewController, UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
+class TeamViewController: UIViewController, UITableViewDelegate,UITableViewDataSource {
     var passedPersons = [TeamPerson]()
-    var collectionView: UICollectionView!
+    var tableView: UITableView = UITableView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        //self.title = "Meet The Team"
+        self.title = "Meet The Team"
         initializeCollectionView()
 
     }
     
-    //Collection View initialization
+    //tableView initialization
     func initializeCollectionView() {
-        let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
-        layout.sectionInset = UIEdgeInsets(top: 20, left: 10, bottom: 10, right: 10)
-        layout.itemSize = CGSize(width: 70, height: 70)
-        
-        let collectionView:UICollectionView = UICollectionView(frame: self.view.frame, collectionViewLayout: layout)
-        collectionView.dataSource = self
-        collectionView.delegate = self
-        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "Cell")
-        collectionView.backgroundColor = UIColor.white
-        self.view.addSubview(collectionView)
+        tableView = UITableView(frame: UIScreen.main.bounds, style: UITableViewStyle.plain)
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        self.view.addSubview(self.tableView)
     }
     
-    //Collection View Setup Delegate and datasource
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    //tableView Setup Delegate and datasource
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return passedPersons.count
     }
     
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath)
-        cell.backgroundColor = UIColor.lightGray
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.subtitle,reuseIdentifier: "cell")
+        cell.textLabel?.text = passedPersons[indexPath.row].firstName + " " + passedPersons[indexPath.row].lastName
+        cell.detailTextLabel?.text = passedPersons[indexPath.row].title
+        cell.imageView?.image = passedPersons[indexPath.row].image
         return cell
     }
+    
 }
